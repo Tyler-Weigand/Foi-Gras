f03ac2ce9816c31cb8050cbc6566d62c230a2c85
kcheng902 <47684688+kcheng902@users.noreply.github.com>
2020-12-04 09:19:51 -0800

    initial var

--- src/goose.java
+++ src/goose.java
@@ -1,9 +1,13 @@
 
-public class goose {
+public class Goose {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n;
+		int l;
+		int[] arr = new int[n];
+		int[][] opt = new[n][l+1];
+		
 	}
 
 }
